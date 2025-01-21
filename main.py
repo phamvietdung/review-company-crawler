9cb293d607f1ed8a5788f81bd48f3f43c39139c6
dungpv <dungpv@creativeforce.io>
2025-01-21 18:38:19 +0700

    update helpers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 
 from helpers import extract_text_from_text_by_pattern, get_hash, write_to_html_file, read_from_file, is_existing_file_in_directory, crawl
+from voz_helpers import clean_comment_text, get_last_page, pre_process_text, get_regex_patterns
 
 url = "https://voz.vn/t/thread-tong-hop-chia-se-ve-muc-luong-tai-cac-cong-ty-part-2.515355/page-2"
 id = get_hash(url)
@@ -17,56 +18,19 @@ else:
     else:
         write_to_html_file(f"{id}.html", soup.prettify())
 
-
-def clean_comment_text(text):
-    import re
-    text = text.replace("\n", ". ")
-    text = text.replace("\r", "- ")
-    text = text.replace("\t", " ")
-    text = re.sub(r'\s{2,}', " ", text)
-    return text
-
-def pre_process_text(text):
-    text = text.replace(". . . .", "\n")
-    text = text.replace("Tên công ty", "\nTên công ty")
-    text = text.replace("Lương tháng/năm", "\nLương tháng/năm")
-    text = text.replace("Vị trí", "\nVị trí")
-    text = text.replace("Thời điểm", "\nThời điểm")
-    text = text.replace("Bonus", "\nBonus")
-    return text
-
-import re
-regex_patterns = [
-    {
-        "name" : "company_name",
-        "regex" : r'Tên\scông\sty:[^\n]*'
-    },
-    {
-        "name" : "salary_gross",
-        "regex" : r'Lương\stháng\/năm[^\n]*'
-    },
-    {
-        "name" : "position",
-        "regex" : r'Vị\strí:[^\n]*'
-    },
-    {
-        "name" : "year",
-        "regex" : r'Thời\tdiểm:[^\n]*'
-    },
-    {
-        "name" : "bonus",
-        "regex" : r'Bonus:[^\n]*'
-    }
-]
-
 if(soup != None):
+
+    last_page = get_last_page(soup)
+
+    print(last_page)
+
     posts = soup.find_all("div", class_="bbWrapper")
     print(len(posts))
     for post in posts:
         # print("-----------------------------")
         comment_text = clean_comment_text(post.get_text())
         comment_text = pre_process_text(comment_text)
-        extract_data = extract_text_from_text_by_pattern(comment_text, regex_patterns)
+        extract_data = extract_text_from_text_by_pattern(comment_text, get_regex_patterns())
         if(len(extract_data) > 0):
             print(extract_data)
         # print(comment_text)
